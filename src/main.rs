81cc8055e8fb60e2741da10c976d6b19fec267a7
Andrzej Klajnert <github@aklajnert.pl>
2020-06-11 21:56:54 +0200

    Extracted method to read path

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,13 @@ use std::io;
 use std::path::Path;
 
 fn main() {
+    let mut definitions_path = String::new();
+    get_path(&mut definitions_path);
+}
+
+fn get_path(mut definitions_path: &mut String) {
     loop {
         println!("Provide path to the redirect definitions file:");
-        let mut definitions_path = String::new();
         io::stdin().read_line(&mut definitions_path).unwrap();
         if !Path::new(&definitions_path.trim()).exists() {
             eprintln!("File doesn't exists.");

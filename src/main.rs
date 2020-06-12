9384dd8862e91e2190783e1336d05644d637d8f0
Andrzej Klajnert <github@aklajnert.pl>
2020-06-12 07:54:55 +0200

    Refactored get_path to return PathBuf

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,22 @@
 use std::io;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 fn main() {
-    let mut definitions_path = String::new();
-    get_path(&mut definitions_path);
+    let path = get_path();
+    println!("Provided path: {:?}", path);
 }
 
-fn get_path(mut definitions_path: &mut String) {
+fn get_path() -> PathBuf {
     loop {
+        let mut definitions_path = String::new();
         println!("Provide path to the redirect definitions file:");
         io::stdin().read_line(&mut definitions_path).unwrap();
-        if !Path::new(&definitions_path.trim()).exists() {
+        let definitions_path = definitions_path.trim();
+        let path = Path::new(&definitions_path);
+        if !path.exists() {
             eprintln!("File doesn't exists.");
         } else {
-            break;
+            return path.to_owned();
         }
     }
 }

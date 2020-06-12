5d3b818e9282ba43ee6a0695fd2e616ebc1f9a3c
Andrzej Klajnert <github@aklajnert.pl>
2020-06-12 16:10:40 +0200

    Create basic function to read the CSV file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
+use std::fmt::Debug;
+use std::fs::File;
 use std::io;
+use std::io::Read;
 use std::path::{Path, PathBuf};
+
+#[derive(Debug)]
+struct RedirectDefinition {
+    name: String,
+    source: String,
+    target: String,
+}
 
 fn main() {
     let path = get_path();
     println!("Provided path: {:?}", path);
+
+    read_csv(path).unwrap();
 }
 
 fn get_path() -> PathBuf {
@@ -19,4 +31,25 @@ fn get_path() -> PathBuf {
             return path.to_owned();
         }
     }
+}
+
+fn read_csv(path: PathBuf) -> std::io::Result<()> {
+    let mut file = File::open(path)?;
+    let mut content = String::new();
+    file.read_to_string(&mut content)?;
+
+    let content = content.to_owned();
+    let mut reader = csv::Reader::from_reader(content.as_bytes());
+    let mut records = vec![];
+    for record in reader.records() {
+        let record = record?;
+        records.push(RedirectDefinition {
+            name: record[0].to_owned(),
+            source: record[1].to_owned(),
+            target: record[2].to_owned(),
+        });
+    }
+    println!("{:?}", records);
+
+    Ok(())
 }
